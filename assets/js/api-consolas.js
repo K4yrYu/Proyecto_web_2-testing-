bf70eb8151e3bc185fc857e9ae9d7df05a717e10
K4yrYu <Vicentepalma1202@gmail.com>
2024-05-24 10:59:27 -0400

    cambio de units a stock

--- assets/js/api-consolas.js
+++ assets/js/api-consolas.js
@@ -4,74 +4,74 @@ export const consola = [
     {
         name: "PS5",
         img: "https://fdsl.cl/imgs-prog/img-consolas/ps5-1.jpeg",
-        stock:"Units Disponibles: 10",
+        stock:"Stock: 10",
         precio: "$550.000"
     },
     {
         name: "PS4",
         img: "https://fdsl.cl/imgs-prog/img-consolas/ps4-1.jpeg",
-        stock:"Units Disponibles: 10",
+        stock:"Stock: 10",
         precio: "$430.000"
     },
     {
         name: "Nintendo Switch",
         img: "https://fdsl.cl/imgs-prog/img-consolas/nintendosw-1.jpeg",
-        stock:"Units Disponibles: 10",
+        stock:"Stock: 10",
         precio: "$450.000"
     },
     {
         name: "Nintendo 3Ds XL",
         img: "https://fdsl.cl/imgs-prog/img-consolas/nds-1.jpeg",
-        stock:"Units Disponibles: 10",
+        stock:"Stock: 10",
         precio: "$120.000"
     },
     {
         name: "Wii",
         //utilizar este tamaño estandar
         img: "https://fdsl.cl/imgs-prog/img-consolas/wii-1.jpeg",
-        stock:"Units Disponibles: 10", 
+        stock:"Stock: 10", 
         precio: "$120.000"
     },
     {
         name: "Steam Deck",
         img: "https://fdsl.cl/imgs-prog/img-consolas/steamd-consola.jpg",
-        stock:"Units Disponibles: 10",
+        stock:"Stock: 10",
         precio: "$156.000"
     },
     {
         name: "Xbox Series X",
         img: "https://fdsl.cl/imgs-prog/img-consolas/consola-xbox-seriesx.jpg",
-        stock:"Units Disponibles: 10", 
+        stock:"Stock: 10", 
         precio: "$550.000"
     },
     {
         name: "PS2",
         img: "https://fdsl.cl/imgs-prog/img-consolas/ps2-consola.jpg",
-        stock:"Units Disponibles: 10",
+        stock:"Stock: 10",
         precio: "$72.000"
     },
     {
         name: "GameBoyAdvance",
         img: "https://fdsl.cl/imgs-prog/img-consolas/gameboya-consola.jpg",
-        stock:"Units Disponibles: 10", 
+        stock:"Stock: 10", 
         precio: "$50.000"
     },
     {
         name: "Sega Génesis",
         img: "https://fdsl.cl/imgs-prog/img-consolas/sega-genesis-consola.jpg",
-        stock:"Units Disponibles: 10", 
+        stock:"Stock: 10", 
         precio: "$36.000"
     },
     {
         name: "Maquina Arcade Clasica",
         img: "https://fdsl.cl/imgs-prog/img-consolas/Maquina-Arcade-Clasica.jpg",
-        stock:"Units Disponibles: 10",
+        stock:"Stock: 10",
         precio: "$400.000"
     },
     {
         name: "Nintendo GameCube",
         img: "https://fdsl.cl/imgs-prog/img-consolas/gamecube-consola.jpg",
-        stock:"Units Disponibles: 10", 
+        stock:"Stock: 10", 
         precio: "$52.000"
     }
 ]
--- assets/js/api-juguetes.js
+++ assets/js/api-juguetes.js
@@ -4,49 +4,49 @@ export const juguete = [
     {
         name: "Amiibo Link",
         img: "https://fdsl.cl/imgs-prog/img-juguetes/Zelda-juguete.png",
-        stock:"Units Disponibles: 20",
+        stock:"Stock: 20",
         precio: "$55.000"
     },
     {
         name: "Amiibo Kirby",
         img: "https://fdsl.cl/imgs-prog/img-juguetes/akirby-juguete.jpg",
-        stock:"Units Disponibles: 20",
+        stock:"Stock: 20",
         precio: "$43.000"
     },
     {
         name: "Amiibo MegaMan",
         img: "https://fdsl.cl/imgs-prog/img-juguetes/megaman1.jpg",
-        stock:"Units Disponibles: 20",
+        stock:"Stock: 20",
         precio: "$45.000"
     },
     {
         name: "Coronel Sanders",
         img: "https://fdsl.cl/imgs-prog/img-juguetes/coronel-juguete.jpg",
-        stock:"Units Disponibles: 20",
+        stock:"Stock: 20",
         precio: "$70.000"
     },
     {
         name: "Funko Pop Kratos",
         img: "https://fdsl.cl/imgs-prog/img-juguetes/Kratos2.jpg",
-        stock:"Units Disponibles: 20",
+        stock:"Stock: 20",
         precio: "$50.000"
     },
     {
         name: "Ammibo Samus",
         img: "https://fdsl.cl/imgs-prog/img-juguetes/samus1.jpg",
-        stock:"Units Disponibles: 20",
+        stock:"Stock: 20",
         precio: "$156.000"
     },
     {
         name: "Batman (Batman vs Superman)",
         img: "https://fdsl.cl/imgs-prog/img-juguetes/Batman-juguete.jpg",
-        stock:"Units Disponibles: 20", 
+        stock:"Stock: 20", 
         precio: "$550.000"
     },
     {
         name: "Funko Pop Spider-Man",
         img: "https://fdsl.cl/imgs-prog/img-juguetes/spiderman-juguete.jpg",
-        stock:"Units Disponibles: 20",
+        stock:"Stock: 20",
         precio: "$62.000"
     }
 ]
