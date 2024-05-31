e7fe32298b03371b6363b878fbee5bdc3476c03d
K4yrYu <Vicentepalma1202@gmail.com>
2024-05-31 11:02:41 -0400

    arreglos ortograficos

---
+++ assets/js/registro-usuarios.js
@@ -0,0 +1,35 @@
+//usuarios principales ADMIN
+
+'use strict';
+export const usuario = [
+    {
+        user: "K4yrYu",
+        contra:"elzapallo",
+        email: "vicep@gmail.com",
+        userRut:"29.720.506-9",
+        Phone:"0987652874",
+        FTperfil:"https://fdsl.cl/imgs-prog/usuarios/zero-user.png",
+        MedioPago: "Visa MasterCard",
+        userDireccion:"las malvinas 420"
+    },
+    {
+        user: "Amuc23",
+        contra:"noctulo",
+        email: "AlonsoUrr@gmail.com",
+        userRut:"29.124.546-k",
+        Phone:"1063873540",
+        FTperfil:"", //subir foto alonso
+        MedioPago: "Visa MasterCard",
+        userDireccion:"las malvinas 420"
+    },
+    {
+        user: "FelipeChavez189",
+        contra:"permatrago",
+        email: "felipeCha@gmail.com",
+        userRut:"31.334.643-8",
+        Phone:"0652691836",
+        FTperfil:"https://fdsl.cl/imgs-prog/usuarios/vader-user.png",
+        MedioPago: "Visa MasterCard",
+        userDireccion:"las malvinas 420"
+    }
+];
